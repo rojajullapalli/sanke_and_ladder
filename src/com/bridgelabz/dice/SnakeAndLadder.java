94f6c421e1d305b0c95eccabd2000e5b296fa2dc
rojajullapalli <rojajules@gmail.com>
2021-08-12 10:00:08 +0530

    player rolls the die to get number 1 to 6

--- src/com/bridgelabz/dice/SnakeAndLadder.java
+++ src/com/bridgelabz/dice/SnakeAndLadder.java
@@ -4,5 +4,7 @@ public class SnakeAndLadder {
     public int position=0;
     public static void main(String[] args) {
         System.out.println("welcome to snake and ladder problem");
+        double diceroll = Math.floor(Math.random() * 10) % 6+1;
+        System.out.println(diceroll);
     }
 }

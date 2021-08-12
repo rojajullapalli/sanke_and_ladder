0e458a57e435052cea4e77f3b32337689986e46e
rojajullapalli <rojajules@gmail.com>
2021-08-12 09:52:01 +0530

    welcome to snake ladder program

---
+++ src/com/bridgelabz/dice/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.dice;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("welcome to snake and ladder program");
+    }
+}

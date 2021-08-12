b3b503236a87b99c17e9ae1407db18253ab581f4
rojajullapalli <rojajules@gmail.com>
2021-08-12 10:14:15 +0530

    check option whether noplay,snake,ladder

--- src/com/bridgelabz/dice/SnakeAndLadder.java
+++ src/com/bridgelabz/dice/SnakeAndLadder.java
@@ -1,10 +1,30 @@
 package com.bridgelabz.dice;
 
 public class SnakeAndLadder {
-    public int position=0;
+    public static int position=0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
     public static void main(String[] args) {
-        System.out.println("welcome to snake and ladder problem");
-        double diceroll = Math.floor(Math.random() * 10) % 6+1;
-        System.out.println(diceroll);
+        int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println("The Value Of Dice is " +diceNumber);
+        int dieCheck = (int) Math.floor(Math.random() * 100) % 3;
+
+        switch (dieCheck) {
+            case LADDER:
+                System.out.println("The player gets Ladder");
+                position += diceNumber;
+                break;
+            case SNAKE:
+                System.out.println("the player gets Snake");
+                position -= diceNumber;
+                if (position <= 0)
+                    position = 0;
+                break;
+            default:
+                System.out.println("The player gets No Play");
+                break;
+        }
+        System.out.println("The Position of Player:" +position);
+
     }
 }

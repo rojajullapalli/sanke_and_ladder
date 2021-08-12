840f73af4ad6acdad7f6cc50431f6c822c0aa98c
rojajullapalli <rojajules@gmail.com>
2021-08-12 09:54:46 +0530

    Snake and Ladder

--- src/com/bridgelabz/dice/SnakeAndLadder.java
+++ src/com/bridgelabz/dice/SnakeAndLadder.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.dice;
 
 public class SnakeAndLadder {
+    public int position=0;
     public static void main(String[] args) {
-        System.out.println("welcome to snake and ladder program");
+        System.out.println("welcome to snake and ladder problem");
     }
 }
